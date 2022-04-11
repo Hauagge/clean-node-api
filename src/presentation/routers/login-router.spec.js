14196d0bf6db7814e534f023691059ea57a67cb2
hauagge <gabriel.dev@agroflux.ind.br>
2022-04-11 14:49:49 -0300

    ensure loginRouter returns 500 is an invalid httpRequest is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,11 @@
 class LoginRouter{
     route(httpRequest){
+        if (!httpRequest || !httpRequest.body) {
+            return {
+                statusCode:500,
+                
+            };
+        }
         const {email, password} = httpRequest.body;
         if(!email || !password){
             return {
@@ -33,6 +39,26 @@ describe('Login Router',()=>{
     }
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toEqual(400);
+
+  })
+
+  
+  it('should return 500 if no httpRequest is provided',async()=>{
+
+    const sut = new LoginRouter();
+   
+    const httpResponse = sut.route();
+    expect(httpResponse.statusCode).toEqual(500);
+
+  })
+
+    
+  it('should return 500 if  httpRequest  has no body',async()=>{
+
+    const sut = new LoginRouter();
+    const httpRequest = { }
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toEqual(500);
 
   })
   
