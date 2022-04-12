b5b38cd31a629950fceaf9c924e313f551c79fc3
hauagge <gabriel.dev@agroflux.ind.br>
2022-04-12 11:34:48 -0300

    ensure LoginRouter return 500 if EmailValidator throws

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -53,6 +53,15 @@ const makeAuthUseCaseWithError = () => {
   return new AuthUseCaseSpy()
 }
 
+const makeEmailValidatorWithError = () => {
+  class EmailValidatorSpy{
+    isValid(){
+      throw new Error();
+    }
+  }
+  return new EmailValidatorSpy()
+}
+
 describe('Login Router', () => {
   it('should return 400 if no email is provided', async () => {
 
@@ -249,5 +258,23 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
   });
+
+  it('should return 500 if   EmailValidator throws', async () => {
+    const authUseCaseSpy =  makeAuthUseCase();
+    const emailValidatorSpy = makeEmailValidatorWithError();
+    authUseCaseSpy.accessToken = 'valid_token'
+    const sut = new LoginRouter(authUseCaseSpy);
+     const httpRequest = {
+       body: {
+         email: 'any_email@email.com',
+         password: 'any_password'
+       }
+ 
+     };
+     const httpResponse = await sut.route(httpRequest);
+     expect(httpResponse.statusCode).toBe(500);
+     expect(httpResponse.body).toEqual(new ServerError());
+ 
+   });
 
 })
